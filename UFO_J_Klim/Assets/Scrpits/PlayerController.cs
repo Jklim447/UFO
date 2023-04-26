4a6c8ebfd406d574e9adad222e5b12aa707ffb5c
Julia Klim <93789683+Jklim447@users.noreply.github.com>
2023-04-26 18:57:08 +0200

    added UI

--- UFO_J_Klim/Assets/Scrpits/PlayerController.cs
+++ UFO_J_Klim/Assets/Scrpits/PlayerController.cs
@@ -1,32 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Diagnostics;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
+    public Text scoreText;
+    public Text winText;
     Rigidbody2D rb2dl;
     // Start is called before the first frame update
     private int count = 0;
+
+    public object ScoreText { get; private set; }
+
     void Start()
     {
         rb2dl = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-        private void FixedUpdate()
+    private void FixedUpdate()
     {
-        float moveHorizontal=Input.GetAxis("Horizontal");
-        float moveVertical=Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-            rb2dl.AddForce(movement * 15);
+        rb2dl.AddForce(movement * 15);
     }
-        
-        private void onTriggerEnder2D(Collider2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Pickup"))
+        if (collision.CompareTag("Pickup"))
         {
             count = count + 1;
             Destroy(collision.gameObject);
+            UpdateScoreText();
         }
     }
+    void UpdateScoreText()
+    {
+        scoreText.text = "Wynik" + count;
+        if (count == 4)
+        {
+            winText.gameObject.SetActive(true);
+            scoreText.gameObject.SetActive(false);
+            SceneManager.LoadScene("Level02");
+        }
+    }
+   
 }
+
+   
